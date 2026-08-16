3e7517ccd24e213b18ec1d0addc668b052aac380
JOVITA <58184053+Jovita2019@users.noreply.github.com>
2026-08-16 12:10:04 +0200

    Change SUPABASE_ANON_KEY in supabase.js
    Updated the SUPABASE_ANON_KEY to a new value.

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,1 @@
-import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = 'https://zyhntpadiipxxeaylxvd.supabase.co'
-const SUPABASE_ANON_KEY = 'sb_publishable_R4EnCHsjI4bu44aUgoe8Dw_cg99ykBg'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp5aG50cGFkaWlweHhlYXlseHZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI0NjAwMTIsImV4cCI6MjA5ODAzNjAxMn0.sKPg-VPXX_dvdWvrlKO6uhvPfLvaz1Z00S46omm8hZQ'
