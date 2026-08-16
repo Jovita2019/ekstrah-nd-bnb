30ec4c22fb2a3bd40fbfa6f63bd52a769d77d266
JOVITA <58184053+Jovita2019@users.noreply.github.com>
2026-08-16 10:03:06 +0200

    Delete ekstrah├Ñnd-bnb directory

--- ekstrah├Ñnd-bnb/src/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const SUPABASE_URL = 'https://zyhntpadiipxxeaylxvd.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp5aG50cGFkaWlweHhlYXlseHZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI0NjAwMTIsImV4cCI6MjA5ODAzNjAxMn0.sKPg-VPXX_dvdWvrlKO6uhvPfLvaz1Z00S46omm8hZQ'
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
